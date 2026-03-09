23970b9469f9a24fda633ca9593dfab1e38becc1
Piioni <rangelc.juane@gmail.com>
2026-03-09 19:28:30 +0100

    Initial commit frontend

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,18 @@
+// @ts-check
+import antfu from '@antfu/eslint-config';
+import withNuxt from './.nuxt/eslint.config.mjs';
+
+export default withNuxt(
+    antfu({
+        vue: true,
+        formatters: true,
+        gitignore: true,
+        stylistic: {
+            indent: 2,
+            quotes: 'single',
+        },
+        rules: {
+            curly: ['error', 'all'],
+        },
+    })
+);
---
+++ frontend/nuxt.config.ts
@@ -0,0 +1,22 @@
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineNuxtConfig({
+  compatibilityDate: '2025-07-15',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ],
+  css: ['./app/assets/css/main.css'],
+
+  vite: {
+    plugins: [
+      // @ts-expect-error - Type incompatibility between Vite 7 and @tailwindcss/vite plugin types
+      tailwindcss()
+    ]
+  }
+
+})
