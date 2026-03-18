b9f47bbfa513a9c4c5a74fe2cefedf3548c0844d
Piioni <rangelc.juane@gmail.com>
2026-03-19 00:39:21 +0100

    Removed unnecesary dependencies

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -4,17 +4,23 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/test-utils',
-    '@nuxt/ui',
+  modules: ['@nuxt/eslint', '@nuxt/image', '@nuxt/ui', '@nuxt/fonts'],
+
+  ui: {
+    colorMode: false,
+  },
+
+  components: [
+    {
+      path: '~/components',
+      pathPrefix: true,
+    },
   ],
+
   css: ['./app/assets/css/main.css'],
 
   vite: {
     plugins: [
-      // @ts-expect-error - Type incompatibility between Vite 7 and @tailwindcss/vite plugin types
       tailwindcss(),
     ],
   },
